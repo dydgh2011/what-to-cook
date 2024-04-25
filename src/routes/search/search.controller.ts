a781446b61c926c4fff8165979028b6850c53c96
Yongho <dydgh2011@gmail.com>
2024-04-26 07:30:49 +0900

    hotfix

--- src/routes/search/search.controller.ts
+++ src/routes/search/search.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Query, Render, Request } from '@nestjs/common';
 import { SearchService } from './search.service';
 import { Public } from 'src/decorators/public.decorator';
 import { ApiTags } from '@nestjs/swagger';
+import { IsPage } from 'src/decorators/page.decorator';
 
 @ApiTags('Search')
 @Controller('search')
@@ -9,6 +10,7 @@ export class SearchController {
   constructor(private readonly searchService: SearchService) {}
   //set the limit of 10 requests per minute
   @Public()
+  @IsPage()
   @Get('')
   @Render('pages/result')
   async search(@Query('search') ingredients: string, @Request() req) {
